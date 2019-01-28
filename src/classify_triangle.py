333e42ec8fda18b37369f41fad05df5112a4f8a1
Kyle <kyle.rozanitis@gmail.com>
2019-01-28 15:54:39 -0500

    Adding /src and classify_triangle.py

---
+++ src/classify_triangle.py
@@ -0,0 +1,6 @@
+""" This program contains two functions, the classify_triange() and valid_input()
+functions. The classify_triangle() function takes three inputs, the three sides
+of a triangle, and returns "Equilateral" if all three sides are equal, "Isoceles"
+if two sides are equal, "Scalene" if none of the sides are equal, "Not A Triangle"
+if the sum of two of the sides is less than the length of the third side, and
+"Right" if the sum of two of the sides equals the square of the remaining side """
