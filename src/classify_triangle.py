ed5cae0f3c8d4bc1b2eff6c698d76678ed6755c7
Kyle <kyle.rozanitis@gmail.com>
2019-01-28 15:55:48 -0500

    Ensuring that repo is tracking properly

--- src/classify_triangle.py
+++ src/classify_triangle.py
@@ -4,3 +4,7 @@ of a triangle, and returns "Equilateral" if all three sides are equal, "Isoceles
 if two sides are equal, "Scalene" if none of the sides are equal, "Not A Triangle"
 if the sum of two of the sides is less than the length of the third side, and
 "Right" if the sum of two of the sides equals the square of the remaining side """
+
+
+def valid_input():
+    
